5cc56433c9f2af374c2db9d05ccc10da50773e33
praveen199 <praveensigh.199@gmail.com>
2020-06-23 20:27:36 +0530

    5.5 : Assigning an Alias to Custom Properties

--- my-first-app/src/app/server-elements/server-elements.component.ts
+++ my-first-app/src/app/server-elements/server-elements.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit,Input } from '@angular/core';
   styleUrls: ['./server-elements.component.css']
 })
 export class ServerElementsComponent implements OnInit {
-  @Input() element: {type: string, name: string, content: string};
+  @Input('srvElement') element: {type: string, name: string, content: string};
 
   constructor() { }
 
