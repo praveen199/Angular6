ed751428de246ed0198ce8f2ba533966a845a0ec
praveen199 <praveensigh.199@gmail.com>
2020-08-16 04:34:02 +0530

    15.18 : TD Setting and Patching Form Values

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -12,6 +12,15 @@ export class AppComponent {
   defaultQuestion = 'teacher';
   answer = ' ';
   genders = ['male','female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted = false;
+  
   suggestUserName() {
     const suggestedName = 'Superuser';
     // this.signupForm.setValue({
@@ -34,6 +43,13 @@ export class AppComponent {
   // }
 
   onSubmit(form : NgForm) {
-    console.log(this.signupForm);
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+
+    this.signupForm.reset();
   }
 }
