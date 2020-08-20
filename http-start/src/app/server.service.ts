ff0e89b4b0fe6a22145df2ee7ab55e228c1dd39a
praveen199 <praveensigh.199@gmail.com>
2020-08-21 01:22:45 +0530

    18.5 : Adjusting Request Headers

--- http-start/src/app/server.service.ts
+++ http-start/src/app/server.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
 
 @Injectable()
 export class ServerService {
  constructor(private http : HttpClient) {}
 
  storeServers(servers : any[]) {
-    return this.http.post('https://udemy-ng-http.firebaseio.com/data.json',servers)
+    const headers = new HttpHeaders({'Content-Type': 'application/json'});
+    return this.http.post('https://udemy-ng-http.firebaseio.com/data.json',servers,{httpHeaders: headers});
  }
 }
